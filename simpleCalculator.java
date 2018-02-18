de3ab4eb2232fb51bece4e99f39eef4b5edad20b
Sujay Hegde <sujay@localhost.localdomain>
2018-02-18 09:46:14 +0530

    Made variables members of the class instead of method main()

--- simpleCalculator.java
+++ simpleCalculator.java
@@ -2,26 +2,31 @@ import java.util.Scanner;
 
 class simpleCalculator
 {
+	double operand1, operand2;
+	char operator;
+
+	// other functions go here
+
     public static void main(String args[])
     {
         Scanner input = new Scanner(System.in);
         System.out.print("expresultsion : ");
-        double a        = input.nextInt();
-        String operator = input.next();
-        double b        = input.nextInt();
+        operand1 = input.nextInt();
+        operator = input.next().charAt(0);
+        operand2 = input.nextInt();
 
         double result;
         switch(operator)
         {
-            case("+") : result = add(a,b);
+            case('+') : result = add(a,b);
                         break;
-            case("-") : result = subtract(a,b);
+            case('-') : result = subtract(a,b);
                         break;
-            case("*") : result = multiply(a,b);
+            case('*') : result = multiply(a,b);
                         break;
-            case("/") : result = divide(a,b);
+            case('/') : result = divide(a,b);
                         break;
-            case("^") : result = expo(a,b);
+            case('^') : result = expo(a,b);
                         break;
         }
 
