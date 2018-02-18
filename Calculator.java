6d0cd8cb8a68248310c35d226c684f14426e2f4d
Subrahmanya-Hegde <subbu.v.h.234@gmail.com>
2018-02-18 11:04:28 +0530

    Java file with all calc functions donegit add Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+class calculator
+{
+    int a,b;
+    calculator(int a,int b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+
+    int addition()
+    {
+        return (a+b);//Addition
+    }
+
+    int subtraction()
+    {
+        return (a-b);//Substraction
+    }
+
+    int multiplication()
+    {
+        return (a*b);//Multiplication
+    }
+
+    int division()
+    {
+        return (a/b);//Division
+    }
+
+}
+
+class calc
+{
+    public static void main(String args[])
+    {
+        int p,r;
+        Scanner s = new Scanner(System.in);
+        while(1)
+        {
+            System.out.println("Enter the 2 numbers :");
+            p = s.nextInt();
+            r = s.nextInt();
+            calculator cal = new calculator(p,r);
+
+            int choice;
+            System.out.println("1 - Addition\n2-Subtraction\3-Multiplication\n4-Division\n5-Exit");
+            choice = s.nextInt();
+
+            switch(choice)
+            {
+                case 1:
+                        cal.addition();
+                        break;
+                case 2:
+                        cal.subtrction();
+                        break;
+                case 3:
+                        cal.multplication();
+                        break;
+                
+                case 4:
+                        cal.division();
+                        break;
+                
+                case 5:
+                        return 0;
+                
+                default:
+                        System.out.println("Enter the correct choice");
+                        break;
+            }
+        }
+    }
+}
